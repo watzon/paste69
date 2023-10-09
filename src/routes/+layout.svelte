59564cdbe6b93e4395d4a51398202d8b1187ee6d
Chris W <cawatson1993@gmail.com>
2023-10-09 10:09:45 -0600

    update tracking code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,20 +6,7 @@
 </script>
 
 <svelte:head>
-	<script defer src={`//analytics.watzon.tech/matomo.js`}></script>
-	<script>
-		var _paq = window._paq = window._paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u="//analytics.watzon.tech/";
-			_paq.push(['setTrackerUrl', u+'matomo.php']);
-			_paq.push(['setSiteId', '1']);
-			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-		})();
-	</script>
+	<script async src="https://analytics.watzon.tech/script.js" data-website-id="542d82bc-7f24-4b96-bc79-b73f2e5f955f"></script>
 </svelte:head>
 
 <Modal />
